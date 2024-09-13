3cc054d04063a0be207a0241f9425619a92f6eb3
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-09-12 23:40:10 -0700

    Fix middleware file location

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,12 @@
 import { AppShell } from "@/components/AppShell";
 import { UserProvider } from "@/components/auth/UserContext";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const supabase = createClient();
-  const user = supabase.auth
-    .getUser()
-    .then((res) => res.data.user || redirect("/login"));
+  const user = supabase.auth.getUser().then((res) => res.data.user!);
 
   return (
     <UserProvider userPromise={user}>
